77a6f6220771c8e9d3898295a755700b85d8e119
turococ <84099957+turococ@users.noreply.github.com>
2026-09-04 02:18:31 +0300

    feat(camera): add camera lag and Post-Process effects
    Add smooth camera rotation with lag in CameraController.cs
    Add lens flare and dirt overlay in Post-Processing

--- Assets/scripts/Player/CameraController.cs
+++ Assets/scripts/Player/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,13 +9,32 @@ public class CameraController : MonoBehaviour
     [SerializeField] float CameraYMax = 89f;
     [SerializeField] float CameraYMin = -89f;
 
+    [SerializeField] float smoothTime = 0.125f;
+
     [SerializeField] Transform cameraTransform;
 
     Vector2 look;
 
+    float yVel;
+    float xVel;
+
+    float currentLookY;
+    float currentLookX;
+
     PlayerInput playerInput;
     InputAction lookAction;
 
+    void Awake()
+    {
+        float initialCameraX = Mathf.DeltaAngle(0, cameraTransform.localEulerAngles.x);
+        float initialPlayerY = Mathf.DeltaAngle(0, transform.localEulerAngles.y);
+
+        look.y = initialCameraX;
+        currentLookY = initialCameraX;
+
+        look.x = initialPlayerY;
+        currentLookX = initialPlayerY;
+    }
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
@@ -33,12 +53,15 @@ public class CameraController : MonoBehaviour
     {
         var lookInput = lookAction.ReadValue<Vector2>();
 
-        look.y += lookInput.y * sensitivity;
+        look.y -= lookInput.y * sensitivity;
         look.x += lookInput.x * sensitivity;
 
         look.y = Mathf.Clamp(look.y, CameraYMin, CameraYMax);
 
-        cameraTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
-        transform.localRotation = Quaternion.Euler(0, look.x, 0);
+        currentLookY = Mathf.SmoothDampAngle(currentLookY, look.y, ref yVel, smoothTime);
+        currentLookX = Mathf.SmoothDampAngle(currentLookX, look.x, ref xVel, smoothTime);
+
+        cameraTransform.localRotation = Quaternion.Euler(currentLookY, 0, 0);
+        transform.localRotation = Quaternion.Euler(0, currentLookX, 0);
     }
 }
